97e0eef632ab0cae5ad871261d7e599fe69354e3
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-02 04:16:31 +0530

    Create InputMismatch.java

---
+++ LA_4.1/InputMismatch.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class InputMismatch {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n,num,sum=0;
+        n=sc.nextInt();
+        for (int i=1;i<=n;i++) {
+            try {
+                num=sc.nextInt();
+                sum+=num;
+            }
+            catch (InputMismatchException e) {
+                System.out.println("ERROR");
+                System.exit(0);
+            }
+        }
+        System.out.println(sum);
+   }
+}

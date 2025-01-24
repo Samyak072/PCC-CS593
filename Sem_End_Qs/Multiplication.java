8d6315dc8dea14be0c6249f336c675682aa58e29
Samyak Mishra <115927401+SamyakMishra072@users.noreply.github.com>
2025-01-24 23:03:41 +0530

    Create Multiplication.java

---
+++ Sem_End_Qs/Multiplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number for the multiplication table: ");
+        int number = scanner.nextInt();
+
+        // Print the multiplication table
+        System.out.println("Multiplication Table for " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+        scanner.close();
+    }
+}
